de17ecc4249a5862359476f6cc7f1f81a95be4c5
Samantha Tissier <stissier@insa-rennes.fr>
2025-10-14 16:17:12 +0200

    Create DameGame

---
+++ dameGame/packages/language/src/index.ts
@@ -0,0 +1,5 @@
+export * from './damedame-module.js';
+export * from './damedame-validator.js';
+export * from './generated/ast.js';
+export * from './generated/grammar.js';
+export * from './generated/module.js';
