4491ab85aa14e7e4b749adf4de7c536a2d9907b0
Katheline <katheline@monolith-paradox.fr>
2025-10-14 17:18:45 +0200

    Remove example project

--- dameGame/packages/language/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './damedame-module.js';
-export * from './damedame-validator.js';
-export * from './generated/ast.js';
-export * from './generated/grammar.js';
-export * from './generated/module.js';
